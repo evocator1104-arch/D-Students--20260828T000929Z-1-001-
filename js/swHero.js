b78e3ca5d109c7f6d1f3113b44a2c9a1af93283a
evocator1104-arch <evocator1104@gmail.com>
2026-09-04 16:59:04 +0900

    feat: 히어로 슬라이드 영역 스와이퍼 라이브러리 작업0904

---
+++ js/swHero.js
@@ -0,0 +1,10 @@
+const swHero = new Swiper(".sw-Hero", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".hero-next-button",
+    prevEl: ".hero-prev-button",
+  },
+});
