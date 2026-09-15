7725db761da39de508d99853bfa7699d41629440
evocator1104-arch <evocator1104@gmail.com>
2026-09-15 17:24:36 +0900

    지역 축제 영역 스와이퍼 구현중

--- js/swHero.js
+++ js/swHero.js
@@ -2,17 +2,19 @@ const mainVisualBg = document.querySelector(
   ".hero .main-visual .main-visual-bg",
 );
 // console.log(mainVisualBg);
-const heroBtns = document.querySelectorAll(".visual-inner button");
+const heroBtns = document.querySelectorAll(".visual-inner .sw-hero-btn");
 // console.log(heroBtns);
-const playStopBtn = document.querySelector();
-(".sw-hero .pagination .play-stop.btn");
+const playStopBtn = document.querySelector(
+  ".sw-hero .pagination .play-stop-btn",
+);
+// console.log(playStopBtn);
 
 // 히어로 슬라이드 버튼 호버 인터랙션
 heroBtns.forEach((btn) => {
   btn.addEventListener("mouseenter", () => {
     btn.querySelector(".off").style.opacity = 1;
   });
-  btn.addEventListener("mouseout", () => {
+  btn.addEventListener("mouseleave", () => {
     btn.querySelector(".off").style.opacity = 0;
   });
 });
@@ -47,13 +49,23 @@ const swHero = new Swiper(".sw-hero", {
     },
   },
 });
+
 // 재생, 정지 버튼
 playStopBtn.addEventListener("click", () => {
-  swHero.autoplay.stop();
-  console.log("슬라이드 오토플레이");
-});
+  // console.log("슬라이드 자동재생 정지");
 
-playStopBtn.style = "재생";
-playStopBtn.textContent = "재생";
-playStopBtn.style = "재생";
-playStopBtn.textContent = "정지";
+  const isRunning = swHero.autoplay.running;
+  // console.log(isRunning);
+
+  if (isRunning) {
+    swHero.autoplay.stop();
+    console.log("자동재생 정지");
+    playStopBtn.style.backgroundImage = `url(assets/icons/slide_play.png)`;
+    playStopBtn.textContent = "재생";
+  } else {
+    swHero.autoplay.start();
+    console.log("자동재생 시작");
+    playStopBtn.style.backgroundImage = `url(assets/icons/slide_stop.png)`;
+    playStopBtn.textContent = "정지";
+  }
+});
